6608818497cbea24983aed48b9bc2bcf44de5565
Dishant <dishanttalwekar@gmail.com>
2026-01-23 12:12:32 +0530

    Add changes

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -11,4 +11,4 @@ userRouter.post('/login', loginUser);
 userRouter.get('/data' , protect,getUserById);
 userRouter.get('/resumes' , protect, getUserResumes);
 
-export default userRouter;
+export default userRouter; 
